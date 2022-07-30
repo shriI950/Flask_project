04cd0b7d814b11366ece3575e9472ddf1a0e9e66
Shrirang Joshi <jshrirang8102001@gmail.com>
2022-07-30 18:08:44 +0530

    v1.0.0
    First version of my flask app

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/welcome")
 def welcome():
     return "This is my first app!!! yay!"
 
+def rootPage():
+    return "This is my root page!"
+
+def introPage():
+    return 'This is Intro page'
 app.run()
